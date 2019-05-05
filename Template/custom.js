98d60baa3f1d98ea1f3f2f55711926f1446311e6
Unknown <Tylerchristensen100@gmail.com>
2019-05-04 19:17:19 -0600

    Mobile fixes

--- Template/custom.js
+++ Template/custom.js
@@ -36,3 +36,14 @@ function smoothScroll() {
             }
         });
 }
+
+
+//mobile navigation closes after item is clicked.
+$(function () {
+    var navMain = $(".navbar-collapse"); // avoid dependency on #id
+    // "a:not([data-toggle])" - to avoid issues caused
+    // when you have dropdown inside navbar
+    navMain.on("click", "a:not([data-toggle])", null, function () {
+        navMain.collapse('hide');
+    });
+});
