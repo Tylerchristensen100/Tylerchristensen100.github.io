e70a3312becd051431a75445168cec50bfa341ce
Tyler <tylerchristensen100@gmail.com>
2021-04-15 19:13:44 -0600

    timout
    removed some loops

--- badUSB/hacked.bat
+++ badUSB/hacked.bat
@@ -2,14 +2,16 @@
 
 
 for /L %%a in (1,1,10) do (
-start cmd.exe
+start cmd.exe 
+timout 1
 )
 
 cd Downloads
-for /L %%a in (1,1,10) do (
-youHaveBeenHacked.jpg
-)
 
-for /L %%a in (1,1,8) do (
+youHaveBeenHacked.jpg
+
+timeout 8
+for /L %%a in (1,1,15) do (
 laughing.gif
+timout 2
 )
