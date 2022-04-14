a38937992bcfa358f23351a6045e891c86327e25
Tyler Christensen <31700358+Tylerchristensen100@users.noreply.github.com>
2022-04-14 10:02:01 -0600

    removed projects
    removed projects because they don't work

--- newsite/src/pages/index.js
+++ newsite/src/pages/index.js
@@ -5,11 +5,6 @@ return (
 	<>
 	<h1>My Projects</h1>
 
-	<ul>
-		<li><a href="./projects/allergenMenu" title="Allergen Menu">Allergen Menu</a></li>
-		<li><a href="./projects/Resume" title="Allergen Menu">Resume - Static/Jquery</a></li>
-		<li><a href="./projects/frostPoint" title="Frost Point">Frost Point</a></li>
-	</ul>
 	</>
 );
 };
