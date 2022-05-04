8d7517f08ed11c1ebd3f26cfac8564b2e9295116
Tyler Christensen <31700358+Tylerchristensen100@users.noreply.github.com>
2022-05-04 12:57:10 -0600

    css
    added color and font

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -3,10 +3,11 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 const Home = () => {
 return (
 	<>
-	<h1>My Projects</h1>
-	<a href="./projects/Resume/index.html" target="_blank">Resume</a><br />
-	<a href="./projects/frostPoint/index.html">Frost Point</a><br />
-	<a href="./projects/badUSB/hacked.bat">Bad USB</a><br />
+		<h1>My Projects</h1>
+		<a href="./projects/Resume/" target="_blank">Resume</a><br />
+		<a href="./projects/frostPoint/"target="_blank">Frost Point</a><br />
+		<a href="./projects/badUSB/hacked.bat" target="_blank">Bad USB</a><br />
+		<a href="./projects/devotional/"target="_blank">Young adult Devotional</a><br />
 	</>
 );
 };
