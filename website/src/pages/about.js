692ba9f1de97b39648ce01c4a0fa189c8088ab63
Tyler Christensen <31700358+Tylerchristensen100@users.noreply.github.com>
2022-06-13 12:24:56 -0600

    updated react-scripts

--- website/src/pages/about.js
+++ website/src/pages/about.js
@@ -1,13 +1,13 @@
-
 import React from "react";
-  
+
 const About = () => {
-  return (
-    <div className="container">
-      <h1 style="color:#0f0f0f;">Tyler Christensen</h1>
-      <h2>Hello, I am a web developer and tech enthusiast.</h2>
-    </div>
-  );
+    return ( <
+        div className = "container" >
+        <
+        h1 style = "color:#0f0f0f;" > Tyler Christensen < /h1> <
+        h2 > Hello, I am a web developer and tech enthusiast. < /h2> <
+        /div>
+    );
 };
-  
+
 export default About;
