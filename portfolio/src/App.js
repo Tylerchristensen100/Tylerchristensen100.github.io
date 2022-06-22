813ca4cdd511013b8a3f50341cd43661ca66ba2e
Tyler Christensen <31700358+Tylerchristensen100@users.noreply.github.com>
2022-06-22 13:52:34 -0600

    new portfolio
    I am creating a new portfolio in react from scratch that will be less buggy and look nicer

---
+++ portfolio/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+import React from "react";
+
+
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </main>
+  );
+}
+
+export default App;
