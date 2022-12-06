9420cf8b2ac0169657ea033cae45088d458a7b14
Tyler Christensen <31700358+Tylerchristensen100@users.noreply.github.com>
2022-12-05 19:10:23 -0700

    search
    Created a search component since that is the next major feature after the date's get sorted...

---
+++ portfolio/public/projects/attendance_system/src/components/Search.js
@@ -0,0 +1,12 @@
+
+function Search() {
+
+return (
+    <>
+        <div className="search">
+            <input type="text" placeholder="Search for a name" />
+        </div>
+    </>
+);
+}
+export default Search;
