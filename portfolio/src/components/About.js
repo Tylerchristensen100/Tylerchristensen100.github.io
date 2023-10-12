c3e71d7e93240ab75768f492ea95545baba43f9f
Tyler Christensen <31700358+Tylerchristensen100@users.noreply.github.com>
2023-10-11 19:43:38 -0600

    Update About.js

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -7,11 +7,11 @@ function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black dark:text-white">
-            Hi, I'm Tyler.
-            <br className="hidden lg:inline-block dark:text-white" />I love to build amazing sites.
+            Tyler Christensen
+            <br className="hidden lg:inline-block dark:text-white" />Front-end Web Developer
           </h1>
           <p className="mb-8 leading-relaxed dark:text-white">
-           I am a passionate web and mobile developer with a passion for building fast and intuitive applications. 
+           I am a passionate web and app developer with a passion for building responive, user focused applications. 
           </p>
           <div className="flex justify-center">
             <a
